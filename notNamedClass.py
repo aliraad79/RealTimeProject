5885514e58dab89abfac06f18eff5ff2c6b16471
raadted <a.ahmadi.k.79@gmail.com>
2023-12-05 22:14:30 +0330

    change reporting

--- notNamedClass.py
+++ notNamedClass.py
@@ -1,5 +1,5 @@
 from algorithm.schedule_algorithm import ScheduleAlgorithm
-from task import Task
+from task import Task, Priority
 
 
 class Scheduler:
@@ -7,36 +7,43 @@ class Scheduler:
         self.algorithm: ScheduleAlgorithm = algorithm
         self.starting_task_list = task_list
         self.task_list = task_list
+        self.timer = -1
+        self.num_done_tasks = 0
+        self.num_expired_tasks = 0
 
     def run(self):
         print(f"Running {self.algorithm}")
-        timer = -1
         expired_tasks = set()
-
 
         while True:
             # next time unit
-            timer += 1
+            self.timer += 1
+            self.update_task_list()
+
             # remove outed tasks
-            for i in self.task_list:
-                if i.is_expired(timer):
-                    expired_tasks.add(i)
-            for i in expired_tasks:
-                if i in self.task_list:
-                    self.task_list.remove(i)
+            self.num_expired_tasks += len([i for i in self.task_list if i.is_expired(self.timer)])
+            self.task_list = [i for i in self.task_list if not i.is_expired(self.timer)]
 
             # break if everything is finished
             if len(self.task_list) == 0:
                 break
+
             # select task
             selected_task = self.algorithm.choose_task(self.task_list)
 
+            # remove task from queue if done
             if selected_task.is_done():
                 self.task_list.remove(selected_task)
+                self.num_done_tasks += 1
                 continue
 
             print("Doing task: ", selected_task)
             selected_task.run()
 
-        print(f"Done EDF in {timer} time unit")
-        print(f"Expired Tasks: {expired_tasks}")
+        print(f"Done EDF in {self.timer} time unit")
+        print(f"Number of done tasks: {self.num_done_tasks}")
+        print(f"Number of Expired Tasks: {self.num_expired_tasks}")
+
+    def update_task_list(self):
+        if self.timer == 5:
+            self.task_list.append(Task(10, 2, Priority.LOW, activation_time=5))
